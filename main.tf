f0a08f9dbaa445e553553be7ed3dee0df562b799
Vamsikrishna Burugadda <65275624+TerraformingCloud@users.noreply.github.com>
2020-08-16 11:19:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,7 @@
 #*                 Root Module                         *#
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
 
-# Remote Backend
 
-terraform {
-    backend "azurerm" {
-        resource_group_name     =   "Jenkins"
-        storage_account_name    =   "tfbackend2020"
-        container_name          =   "tfremote"
-        key                     =   "terraform.tfstate"
-    }
-}
 
 # Provider Block
 
@@ -22,35 +13,9 @@ provider "azurerm" {
     subscription_id =   var.subscription_id
     tenant_id       =   var.tenant_id
     
-    features {
-        virtual_machine {
-            delete_os_disk_on_deletion = true
-        }
-    }
+    features {}
 }
 
-provider "azuread" {
-    version         =   ">= 0.11"
-    client_id       =   var.client_id
-    client_secret   =   var.client_secret
-    subscription_id =   var.subscription_id
-    tenant_id       =   var.tenant_id
-    alias           =   "ad"
-}
 
-// module "winvm" {
-//     source = "./azure/windowsVM"
-// }
 
-// module "customrole" {
-//     source  =   "./azure/custom-roles"
-// }
-
-// module "provisioners" {
-//     source  =   "./azure/provisioners-example"
-// }
-
-// module "linuxvm" {
-//     source  =   "./azure/linuxVM"
-// }
 
